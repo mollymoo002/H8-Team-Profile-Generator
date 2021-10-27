cb76040f6ed804bae7c72b98b48e60a42176ff13
Molly Singmaster <mollysingmaster@gmail.com>
2021-10-27 16:28:51 -0400

    added the prompts for the user

--- app.js
+++ app.js
@@ -4,3 +4,37 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
+const employees = [];
+
+function initApp() {
+    startHTML();
+    addMember();
+}
+
+function addMember() {
+    inquirer.prompt([{
+        message: "Enter the member's name",
+        name: "name"
+    },
+    {
+        type: "list",
+        message: "Select the member's role",
+        choices: [
+            "Engineer",
+            "Intern",
+            "Manager"
+        ],
+        name: "role"
+    },
+    {
+        message: "enter the member's ID",
+        name: "id"
+    },
+    {
+        message: "Enter member's email",
+        name: "email"
+    }])
+    .then (function({ name, role, id, email}) {
+        
+    })
+}
