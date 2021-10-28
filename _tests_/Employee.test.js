b741c9e0523ff7dd03729595b73d05af9637207f
Molly Singmaster <mollysingmaster@gmail.com>
2021-10-28 13:56:49 -0400

    added the describe keywork functionality to the employee test file

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -25,5 +25,35 @@ describe("employee", () => {
         expect(e.id).toBe(testVal);
     });
 
-    
+    describe("getName", () => {
+        it("Should get a name via getName()", () => {
+            const testVal = "Molly";
+            const e = new Employee(testVal);
+            expect(e.getName().toBe(testVal));
+        });
+    });
+
+    describe("getId", () => {
+        it("Should get an id via getId()", () => {
+            const testVal = 100;
+            const e = new Employee("Olivia", testVal);
+            expect(e.getId()).toBe(testVal);
+        });
+    });
+
+    describe("getEmail", () => {
+        it("Should get email via getEmail()", () => {
+            const testVal = "test@test.com";
+            const e = new Employee("Olivia", 1, testVal);
+            expect(e.getEmail()).toBe(testVal);
+        });
+    });
+
+    describe("getRole", () => {
+        it("Should have getRole() return Employee", () => {
+            const testVal = "Employee";
+            const e = new Employee("Molly", 1, "test@test.com");
+            expect(e.getRole()).toBe(testVal);
+        });
+    });
 });
