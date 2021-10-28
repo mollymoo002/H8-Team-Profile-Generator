e3dd8a3c6a2a14adf349ff9bee3621367de7d92c
Molly Singmaster <mollysingmaster@gmail.com>
2021-10-28 14:18:14 -0400

    added test for managers

---
+++ _tests_/Manager.test.js
@@ -0,0 +1,20 @@
+const { test } = require("@jest/globals");
+const Manager = require("../lib/Manager");
+
+test("Should set the office number from constructor", () => {
+    const testVal = 100;
+    const e = new Manager("Molly", 1, "test@test.com", testVal);
+    expect (e.getRole()).toBe(testVal);
+});
+
+test("Should have getRole() return Manager", () => {
+    const testVal = "Manager";
+    const e = new Manager("Molly", 1, "test@test.com", 100);
+    expect(e.getOfficeNumber()).toBe(testVal);
+});
+
+test("Should get office number from getOffice()", () => {
+    const testVal = 100;
+    const e = new Manager("Molly", 1, "test@test.com", testVal);
+    expect(e.getOfficeNumber()).toBe(testVal);
+});
